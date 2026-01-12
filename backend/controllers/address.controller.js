8b4ec92fd034b21a55c52d7c84701d5779d5b1b4
parshantsaini <sainiparshant1901@gmail.com>
2026-01-12 09:58:20 +0530

    Enhance address management: add setDefaultAddress functionality and update routes

--- backend/controllers/address.controller.js
+++ backend/controllers/address.controller.js
@@ -4,186 +4,221 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import Address from "../models/address.model.js";
 import mongoose from "mongoose";
 
+const addAddress = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const {
+    fullName,
+    phone,
+    address,
+    city,
+    state,
+    pinCode,
+    country,
+    addressType,
+    isDefault,
+  } = req.body;
 
+  if (!userId) {
+    throw new ApiError(401, "unauthorised Access");
+  }
 
-const addAddress = asyncHandler( async(req,res) =>{
+  if (
+    !fullName ||
+    !phone ||
+    !address ||
+    !city ||
+    !state ||
+    !country ||
+    !pinCode
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-    const userId = req.user?._id;
-    const { fullName, phone, address, city, state, pinCode, country, addressType, isDefault} = req.body;
+  if (isDefault) {
+    await Address.updateMany(
+      { user: userId, isDefault: true },
+      { isDefault: false }
+    );
+  }
 
-    if(!userId){
-        throw new ApiError(401, "unauthorised Access");
-    }
+  const addr = await Address.create({
+    user: userId,
+    fullName,
+    phone,
+    address,
+    city,
+    state,
+    pinCode,
+    country,
+    addressType,
+    isDefault,
+  });
 
-    if(!fullName || !phone || !address || !city || !state || !country  || !pinCode ){
-        throw new ApiError(400, "All fields are required")
-    }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Address added successfully", true, addr));
+});
 
-    if(isDefault){
-        await Address.updateMany(
-            { user: userId, isDefault: true },
-            { isDefault: false}
-        );
-    }
+const getAddress = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized Access");
+  }
 
-    const addr = await Address.create({
-        user: userId,
-        fullName,
-        phone,
-        address,
-        city,
-        state,
-        pinCode,
-        country,
-        addressType,
-        isDefault
+  const addresses = await Address.find({ user: userId })
+    .sort({ isDefault: -1, createdAt: -1 })
+    .lean();
+
+  if (!addresses || !addresses.length === 0) {
+    throw new ApiError(404, "Address not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Addresses fetched successfully", true, addresses)
+    );
+});
+
+const deleteAddress = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const { id } = req.params;
+
+  if (!userId) {
+    throw new ApiError(401, "unauthorised Access");
+  }
+
+  if (!id) {
+    throw new ApiError(400, "Address Id is required");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(404, "Invalid addressId");
+  }
+
+  const address = await Address.find({
+    _id: id,
+    user: userId,
+  });
+
+  if (!address) {
+    throw new ApiError(404, "Address not Found");
+  }
+
+  const wasDefault = address.isDefault;
+
+  await Address.deleteOne({ _id: id });
+
+  if (wasDefault) {
+    const nextAddress = await Address.findOne({ user: userId }).sort({
+      createdAt: -1,
     });
+    if (nextAddress) {
+      nextAddress.isDefault = true;
+      await nextAddress.save();
+    }
+  }
 
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Address deleted successfully", true));
+});
 
-    return res
+const updateAddress = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const { id } = req.params;
+
+  if (!userId) {
+    throw new ApiError(401, "unauthorised Access");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(409, "Invalid Address Id");
+  }
+
+  const {
+    fullName,
+    phone,
+    address,
+    city,
+    state,
+    pinCode,
+    country,
+    addressType,
+    isDefault,
+  } = req.body;
+
+  const updateData = {};
+  if (fullName) updateData.fullName = fullName;
+  if (phone) updateData.phone = phone;
+  if (address) updateData.address = address;
+  if (city) updateData.city = city;
+  if (state) updateData.state = state;
+  if (pinCode) updateData.pinCode = pinCode;
+  if (country) updateData.country = country;
+  if (addressType) updateData.addressType = addressType;
+  if (typeof isDefault === "boolean") updateData.isDefault = isDefault;
+
+  if (isDefault) {
+    await Address.updateMany(
+      { user: userId, isDefault: true },
+      { isDefault: false }
+    );
+  }
+
+  const updatedAddress = await Address.findOneAndUpdate(
+    { _id: id, user: userId },
+    updateData,
+    { new: true }
+  );
+
+  return res
     .status(200)
     .json(
-        new ApiResponse(
-            200,
-            "Address added successfully",
-            true,
-            addr
-        )
+      new ApiResponse(200, "Address Updated Successfully", true, updatedAddress)
     );
 });
 
-const getAddress = asyncHandler( async(req,res) =>{
+const setDefaultAddress = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const { id } = req.params;
 
-    const userId = req.user?._id;
-    if(!userId){
-        throw new ApiError(401, "Unauthorized Access");
-    }
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized Access");
+  }
 
-    const addresses = await Address.find({user: userId})
-        .sort({ isDefault: -1, createdAt: -1, })
-        .lean();
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(400, "Invalid Address Id");
+  }
 
-    if(!addresses || !addresses.length === 0){
-        throw new ApiError(404, "Address not found")
-    }
+  const { isDefault } = req.body;
 
-    return res
+  if (typeof isDefault !== "boolean") {
+    throw new ApiError(400, "Invlaid value or missing");
+  }
+
+  if (isDefault === true) {
+    await Address.updateMany(
+      { user: userId, isDefault: true },
+      { isDefault: false }
+    );
+  }
+
+  const updatedAddr = await Address.findOneAndUpdate(
+    { _id: id, user: userId },
+    { isDefault },
+    { new: true }
+  );
+
+  return res
     .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            "Addresses fetched successfully",
-            true,
-            addresses
-        )
-    );
-});
-
-const deleteAddress = asyncHandler( async(req,res) =>{
-    
-    const userId = req.user?._id;
-    const { id } = req.params;
-
-    if(!userId){
-        throw new ApiError(401, "unauthorised Access");
-    }
-
-    if(!id){
-        throw new ApiError(400, "Address Id is required");
-    }
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        throw new ApiError(404, "Invalid addressId")
-    }
-
-    const address = await Address.find({
-        _id:id,
-        user: userId
-    });
-
-    if(!address){
-        throw new ApiError(404, "Address not Found");
-    }
-
-    const wasDefault = address.isDefault;
-
-    await Address.deleteOne({_id: id});
-
-    if(wasDefault){
-        const nextAddress = await Address.findOne({user: userId}).sort({createdAt: -1});
-        if(nextAddress){
-            nextAddress.isDefault = true;
-            await nextAddress.save();
-        }
-    }
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            "Address deleted successfully",
-            true
-        )
-    );
-
-});
-
-const updateAddress =asyncHandler( async(req,res) =>{
-
-    const userId = req.user?._id;    
-    const {id} = req.params;
-
-    if(!userId){
-        throw new ApiError(401, "unauthorised Access");
-    }
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        throw new ApiError(409,"Invalid Address Id");
-    }
-
-    const { fullName, phone, address, city, state, pinCode, country, addressType, isDefault} = req.body;
-
-    const updateData = {};
-    if (fullName) updateData.fullName = fullName;
-    if (phone) updateData.phone = phone;
-    if (address) updateData.address = address;
-    if (city) updateData.city = city;
-    if (state) updateData.state = state;
-    if (pinCode) updateData.pinCode = pinCode;
-    if (country) updateData.country = country;
-    if (addressType) updateData.addressType = addressType;
-    if (typeof isDefault === "boolean") updateData.isDefault = isDefault;
-
-    if(isDefault){
-        await Address.updateMany(
-            { user: userId, isDefault: true },
-            { isDefault: false}
-        );
-    }
-
-    const updatedAddress = await Address.findOneAndUpdate(
-        {_id: id, user: userId},
-        updateData,
-        { new: true}
-    );
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            "Address Updated Successfully",
-            true,
-            updatedAddress
-        )
-    );
-
+    .json(new ApiResponse(200, "Default Address changed", true, updatedAddr));
 });
 
 export {
-    addAddress,
-    getAddress,
-    deleteAddress,
-    updateAddress
-}
+  addAddress,
+  getAddress,
+  deleteAddress,
+  updateAddress,
+  setDefaultAddress,
+};
--- backend/routes/address.routes.js
+++ backend/routes/address.routes.js
@@ -1,6 +1,12 @@
-import {Router} from "express";
+import { Router } from "express";
 import { verifyJwt } from "../middleware/auth.middleware.js";
-import { addAddress, deleteAddress, getAddress, updateAddress } from "../controllers/address.controller.js";
+import {
+  addAddress,
+  deleteAddress,
+  getAddress,
+  setDefaultAddress,
+  updateAddress,
+} from "../controllers/address.controller.js";
 
 const router = Router();
 
@@ -8,5 +14,6 @@ router.post("/add-addr", verifyJwt, addAddress);
 router.get("/get-addr", verifyJwt, getAddress);
 router.delete("/remove-addr/:id", verifyJwt, deleteAddress);
 router.patch("/update-addr/:id", verifyJwt, updateAddress);
+router.patch("/update-default/:id", verifyJwt, setDefaultAddress);
 
 export default router;
